e8d63ba5478bb6310fcb88b68a3322c77006ee34
akshay0599 <165309890+akshay0599@users.noreply.github.com>
2025-07-12 22:01:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ window.addEventListener('scroll', () => {
   const scrolled = window.scrollY;
   document.querySelector('.hero').style.backgroundPositionY = -(scrolled * 0.3) + 'px';
 });
+<script>
+  const viewer = document.querySelector("model-viewer");
+  viewer.addEventListener("load", () => {
+    document.getElementById("loader").style.display = "none";
+  });
+</script>
