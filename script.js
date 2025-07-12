88cc08e0e49dcf579c50fdd0653c53acb2b53659
akshay0599 <165309890+akshay0599@users.noreply.github.com>
2025-07-12 16:20:33 +0530

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-// Add parallax scroll effect on hero background
-window.addEventListener('scroll', () => {
-  const scrolled = window.scrollY;
-  document.querySelector('.hero').style.backgroundPositionY = -(scrolled * 0.3) + 'px';
-});
